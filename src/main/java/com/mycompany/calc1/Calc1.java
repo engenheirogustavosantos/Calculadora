f56058771179fe0fa9eebd7705673b1ec49d143f
Gustavo5ant0s <gustavo.cssantos2002@gmail.com>
2022-09-12 11:28:28 -0300

    Calculadora 2.0

--- src/main/java/com/mycompany/calc1/Calc1.java
+++ src/main/java/com/mycompany/calc1/Calc1.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Calc1 {
 
     public static void main(String[] args) {
-        int op, num, num2, res = 0;
+        float op, num, num2, res = 0;
         
-        System.out.println("Escolha a opção: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Subtração");
+        System.out.println("Escolha a opção: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
         Scanner esc = new Scanner(System.in);
         op = esc.nextInt();
         
-        
+        //Soma.
             if(op == 1){
                  System.out.println("Digite aqui o primeiro valor: ");
                  Scanner vlr = new Scanner(System.in);
@@ -25,6 +25,7 @@ public class Calc1 {
                 res = (num + num2);
             }
             
+            //Subtração.
             if(op == 2){
                  System.out.println("Digite aqui o primeiro valor: ");
                  Scanner vlr = new Scanner(System.in);
@@ -38,6 +39,7 @@ public class Calc1 {
                 res = (num - num2);
             }
             
+            //Multiplicação
             if(op == 3){
                  System.out.println("Digite aqui o primeiro valor: ");
                  Scanner vlr = new Scanner(System.in);
@@ -51,6 +53,7 @@ public class Calc1 {
                 res = (num * num2);
             }
             
+            //Divisão.
             if(op == 4){
                  System.out.println("Digite aqui o primeiro valor: ");
                  Scanner vlr = new Scanner(System.in);
@@ -63,6 +66,6 @@ public class Calc1 {
                  
                 res = (num / num2);
             }
-        System.out.println("O resulado é: "+res);
+        System.out.println("O resultado é: "+res);
     }
 }
